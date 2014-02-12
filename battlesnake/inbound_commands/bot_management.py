cba49bfe0eb8c91b23abb511461b5fac020de962
Greg Taylor <gtaylor@l11solutions.com>
2014-02-11 22:27:14 -0500

    Adding delimiter and prefix details to botinfo.

--- battlesnake/inbound_commands/bot_management.py
+++ battlesnake/inbound_commands/bot_management.py
@@ -24,9 +24,15 @@ class BotInfoCommand(BaseCommand):
             "============ Battlesnake Botinfo ============\r"
             " Bot name: {bot_name}\r"
             " Bot DBref: {bot_dbref}\r"
+            " Command Prefix: {cmd_prefix}\r"
+            " Command Kwarg Delim: {cmd_kwarg_delimiter}\r"
+            " Command Kwarg List Delim: {cmd_kwarg_list_delimiter}\r"
             "============================================="
         ).format(
             bot_name=bot_name,
-            bot_dbref=bot_dbref
+            bot_dbref=bot_dbref,
+            cmd_prefix=protocol.cmd_prefix,
+            cmd_kwarg_delimiter=protocol.cmd_kwarg_delimiter,
+            cmd_kwarg_list_delimiter=protocol.cmd_kwarg_list_delimiter,
         )
         mux_commands.pemit(protocol, invoker_dbref, pval)
